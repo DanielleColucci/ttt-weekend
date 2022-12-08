9b8cae1f709e5614d605d4260cd8150dbc2220d6
DanielleColucci <ddcolucci99@gmail.com>
2022-12-08 16:12:02 -0500

    create winningCombos constant

--- js/app.js
+++ js/app.js
@@ -17,7 +17,16 @@
 */
 
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8], 
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [0, 4, 8], 
+  [2, 4, 6]
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -142,8 +151,8 @@ init()
   ////     - Otherwise, render a congratulatory message to the player that has 
   ////       won.
 
-  // 4f) Invoke both the `updateBoard` and the `updateMessage` functions
-  //     inside of your `render` function.
+  //// 4f) Invoke both the `updateBoard` and the `updateMessage` functions
+  ////     inside of your `render` function.
 
 // Step 5 - Define the required constants
 
