17b1c9cb807d473a83a5cc809641eb6554cc12ff
DanielleColucci <ddcolucci99@gmail.com>
2022-12-08 15:36:31 -0500

    create and call init function to render game state

--- js/app.js
+++ js/app.js
@@ -34,8 +34,18 @@ const messageEl = document.getElementById('message')
 
 
 /*-------------------------------- Functions --------------------------------*/
+function init() {
+  board = [null, null, null, null, null, null, null, null, null]
+  turn = 1
+  winner = false
+  tie = false
+  render()
+}
 
 
+
+// Game play: 
+init()
 
 //// Step 1 - Define the required variables used to track the state of the game
 
@@ -49,32 +59,32 @@ const messageEl = document.getElementById('message')
 ////   1d) Use a variable named `tie` to represent if the game has ended in a tie.
 
 
-// Step 2 - Store cached element references.
+//// Step 2 - Store cached element references.
 
-  // 2a) In a constant called `squareEls`, store the nine elements 
-  //    representing the squares on the page.
+  //// 2a) In a constant called `squareEls`, store the nine elements 
+  ////    representing the squares on the page.
 
-  // 2b) In a constant called `messageEl`, store the element that displays the 
-  //    game's status on the page.
+  //// 2b) In a constant called `messageEl`, store the element that displays the 
+  ////    game's status on the page.
 
 
-// Step 3 - Upon loading, the game state should be initialized, and a function 
-//          should be called to render this game state.
+//// Step 3 - Upon loading, the game state should be initialized, and a function 
+////          should be called to render this game state.
 
-  // 3a) Create a function called `init`.
+  //// 3a) Create a function called `init`.
 
-  // 3b) Call this `init` function when the app loads.
+  //// 3b) Call this `init` function when the app loads.
   
-  // 3c) Set the `board` variable to an array containing nine `null`s to 
-  //    represent empty squares.
+  //// 3c) Set the `board` variable to an array containing nine `null`s to 
+  ////    represent empty squares.
 
-  // 3d) Set the `turn` to `1` - which will represent player X.
+  //// 3d) Set the `turn` to `1` - which will represent player X.
 
-  // 3e) Set the `winner` to false.
+  //// 3e) Set the `winner` to false.
 
-  // 3f) Set `tie` to false.
+  //// 3f) Set `tie` to false.
 
-  // 3g) Call a function called `render` at the end of the `init` function.
+  //// 3g) Call a function called `render` at the end of the `init` function.
 
 
 // Step 4 - The state of the game should be rendered to the user
